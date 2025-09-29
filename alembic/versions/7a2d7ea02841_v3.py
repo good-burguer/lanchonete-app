be8c9ca6fd7ba2e9a767a1155eb5929010348cd9
Danilo Casabona <danilocasabona>
2025-09-29 17:40:32 -0300

    chore(ci): adiciona debug de logs e eventos no job db-migrate2

--- alembic/versions/7a2d7ea02841_v3.py
+++ alembic/versions/7a2d7ea02841_v3.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '7a2d7ea02841'
-down_revision: Union[str, None] = 'ae2ef90a907f'
+down_revision: Union[str, None] = '3f13625e81b2'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
